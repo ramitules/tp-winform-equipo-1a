b741c65dff54814c7b0ea3c6a3d7e81614942266
ramitules <ramirotules@hotmail.com>
2026-04-15 21:58:09 -0300

    Nueva clase "Articulo"

---
+++ Articulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_1A
+{
+  internal class Articulo
+  {
+    public int ID { get; set; }
+
+    private string codArticulo;
+    public string CodArticulo
+    {
+      set
+      {
+        if (value.Length < 50)
+        {
+          codArticulo = value;
+        }
+      }
+      get { return codArticulo; }
+    }
+
+    private string nombre;
+    public string Nombre
+    {
+      set
+      {
+        if (value.Length < 50)
+        {
+          nombre = value;
+        }
+      }
+      get { return nombre; }
+    }
+    
+  }
+}
