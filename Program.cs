7840fdb62859c011b6f7c2fb70c2ae7deb61ca04
ramitules <ramirotules@hotmail.com>
2026-04-15 21:09:19 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_1A
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
