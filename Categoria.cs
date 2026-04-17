7d85eef253ca4e430e2355b74b4b847f4d720b01
Matias Paz <matiasdanielpaz@outlook.com.ar>
2026-04-17 17:09:40 -0300

    Creación de la clase Categoria

---
+++ Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_1A
+{
+    internal class Categoria
+    {
+        public int Id { get; set; }
+        
+        private string descripcion;
+        public string Descripcion 
+        { 
+            get  { return descripcion; }
+
+            set
+            { 
+                if (value.Length < 50)
+                {
+                    descripcion = value;
+                }
+            }
+        }
+}
+}
