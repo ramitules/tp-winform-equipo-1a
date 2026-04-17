0b74d724ab6723265cc87cdc1aab8675322262b6
Miguel <fernandezmonzonmiguel@gmail.com>
2026-04-17 00:48:47 -0300

    Clase Marca: Se creó la clase con sus atributos id y nombre.

---
+++ Marca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_1A
+{
+    internal class Marca
+    {
+        private int id;
+        public int Id { get; set; }
+
+        private string nombre;
+        public string Nombre {
+            set
+            {
+                if (value.Length < 50)
+                {
+                    nombre = value;
+                }
+            }
+            get { 
+                return nombre; 
+            }
+        
+        
+        }
+    }
+}
