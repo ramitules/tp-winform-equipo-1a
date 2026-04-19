398a634f3d2b34f4d340a745dea0368956b921d3
Matias Paz <matiasdanielpaz@outlook.com.ar>
2026-04-19 14:27:27 -0300

    Creada la clase Negocio con un metodo listar que crea toda la conexion a la base de datos y trae todos los articulos con sus propiedades y devuelve una lista

--- Datos.cs
+++ Datos.cs
@@ -10,17 +10,7 @@ namespace TPWinForm_equipo_1A
 {
     internal class Datos
     {
-        string connectionString;
-        SqlConnection conexion;
-        SqlCommand comando;
-        SqlDataReader lector;
         
-        public Datos()
-        {
-            connectionString = ConfigurationManager.ConnectionStrings["TPWinForm_equipo_1A"].ConnectionString;
-            conexion = new SqlConnection(connectionString);
-            comando = new SqlCommand();
-        }
 
     }
 }
